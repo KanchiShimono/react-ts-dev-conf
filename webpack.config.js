98333f5e1eaf5c00021cfaf666c08ce1b5c34410
Kanchi Shimono <shimono-kanchi-yc@ynu.jp>
2019-05-04 23:55:08 +0900

    Add a webpack conf file

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, './src/index.tsx'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    port: 3000,
+    publicPath: '/',
+  },
+  devtool: 'eval-source-map',
+  // change production when build for production environment
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            loader: 'ts-loader',
+          },
+          {
+            test: /\.css$/,
+            loaders: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/,
+            exclude: /node_modules/,
+            loader: 'url-loader',
+            options: {
+              limit: 20000,
+              name: '[name].[ext]',
+            },
+          },
+          {
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
